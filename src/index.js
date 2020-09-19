5620a5e8af1729ce6b7046ff9be0b16f46d67bd4
kafeingyilmaz <gungor.yilmaz@vodafone.com>
2020-09-19 22:42:31 +0300

    react-create-app on Heroku

--- src/index.js
+++ src/index.js
@@ -1,17 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+mapboxgl.accessToken = 'pk.eyJ1IjoibmVkZW5lbWUiLCJhIjoiY2tmNXhiYjJyMHI1ZzJxbnk0aWxkbHU2NyJ9.pJUZ9n_xkffXL_tcwsSGyQ';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var markers = [];
+
+var serviceUrl = process.env.REACT_APP_SERVICE_URL;
+
+
+class Application extends React.Component {
+  constructor(props) {
+  super(props);
+    this.state = {
+      lng: 13.403,
+      lat: 52.53,
+      zoom: 10
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+	
+	// refers to drag
+	map.on('moveend', () => {
+      console.log("move ended");
+    });
+	
+	map.on('load', function () {
+		console.log('onload worked');
+		
+		window.setInterval(function () {
+            
+			fetch( serviceUrl+'/vehicles/54/15/52/12')
+			.then(res => res.json())
+			.then((data) => {
+				
+			
+				
+				if (markers !== null) {
+					for (var i = markers.length - 1; i > -1; i--) {
+					  markers[i].remove();
+					}
+					markers = [];
+				}
+				
+				console.log(data.vehicleList);
+				let vehicles = data.vehicleList;
+				
+				
+				let features = [];
+				
+				for(var i = 0; i < vehicles.length; i++) {
+					var vehicle = vehicles[i];
+					
+					features.push(  
+						{
+							'type': 'Feature',
+							'geometry': {
+								'type': 'Point',
+								'coordinates': [vehicle.locations[0].lng , vehicle.locations[0].lat]
+							}
+						}
+					);
+				}
+				
+				features.forEach(function(marker) {
+
+				    // create a HTML element for each feature
+				    var el = document.createElement('div');
+				    el.className = 'marker';
+
+				    console.log(marker.geometry.coordinates);
+				    // make a marker for each feature and add to the map
+				    marker = new mapboxgl.Marker(el)
+						.setLngLat(marker.geometry.coordinates)
+						.addTo(map);
+					markers.push(marker);
+				});
+				
+				//map.triggerRepaint();
+				
+			})
+			.catch(console.log)
+			
+         }, 200);
+		
+	        
+    });
+	
+  }
+
+  render() {
+    return (
+      <div>
+        <div ref={el => this.mapContainer = el} className='mapContainer' />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
